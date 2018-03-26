8766b02fd6c83cd22f5160cd46f535bc387f11ba
amaurel <alexandre.maurel@gmail.com>
2018-03-26 13:27:01 +0200

    upgrade log to new tercen lib

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 
-rm -r packrat
-rm .Rprofile
+rm -r packrat && rm .Rprofile && R --vanilla -e "packrat::init(options = list(use.cache = TRUE))"
 
-R --vanilla -e "packrat::init(options = list(use.cache = TRUE))"
+
 
 git add -A && git commit -m "tercen lib upgrade" && git tag -a 0.2.0 -m "++" && git push && git push --tags
