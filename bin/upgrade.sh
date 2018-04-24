71bd47c08ea1c427f2383f79fbd7a5a23bba8688
amaurel <alexandre.maurel@gmail.com>
2018-04-24 16:16:09 +0200

    upgrade tercenlib : default serialisation to tson

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -5,4 +5,4 @@ rm -r packrat && rm .Rprofile && R --vanilla -e "packrat::init(options = list(us
 
 
 
-git add -A && git commit -m "nloptr upgrade" && git tag -a 0.1.2 -m "++" && git push && git push --tags
+git add -A && git commit -m "nloptr upgrade" && git tag -a 0.1.3 -m "++" && git push && git push --tags
