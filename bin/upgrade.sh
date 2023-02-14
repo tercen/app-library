27a7bc70549b1c6f28129aae7f53676f400ea715
Alexandre Gouy <alexandre.gouy@gmail.com>
2023-02-14 13:38:53 +0100

    removed old dependencies

--- bin/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-rm -r packrat && rm .Rprofile && R --vanilla -e "packrat::init(options = list(use.cache = TRUE))"
-
-
-
-git add -A && git commit -m "packrat added" && git tag -a 0.1.6 -m "++" && git push && git push --tags
